10f9df8f56f5251e4772ec6e0c9e0b4c95105add
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-03-13 17:14:40 +0100

    Feat: create file html and js in folder 'For' + create an 'ul' in body html

---
+++ for/js/script.js
@@ -0,0 +1,18 @@
+// Data una lista della spesa,
+// Stampare sulla pagina (anche brutalmente, basta che si vedano)
+// gli elementi della lista individualmente.
+// Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
+
+// Dato l'array della lista della spesa
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+
+
+
+
